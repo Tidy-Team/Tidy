d7ee8d2584790fe90d327a734e2fc2f9bec92480
Gayoso Facundo <facusbrr@gmail.com>
2024-09-12 11:36:40 -0300

    Realizacion de mejoras de la base de datos con pool

--- backend/src/config/databases.js
+++ backend/src/config/databases.js
@@ -1,10 +1,16 @@
-import { Sequelize } from "sequelize";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./env.js";
+import { Sequelize } from 'sequelize';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './env.js';
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: "mysql",
+  dialect: 'mysql',
   port: DB_PORT,
+  pool: {
+    max: 10, //Mantiene hasta 10 conexiones simultáneamente
+    min: 0, //No mantiene ninguna conexión cuando no se necesitan
+    acquire: 30000, //Intenta una conexión durante un máximo de 30 segundos
+    idle: 10000, //Una conexión debe tener un máximo de 10 segundos antes de ser liberada
+  },
   logging: false, //Desactiva el filtro de consultas SQL. Aumenta el rendimiento de la aplicación.
 });
 
@@ -12,7 +18,7 @@ export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 const testConnect = async () => {
   try {
     await sequelize.authenticate();
-    console.log("La conexión se estableció correctamente");
+    console.log('La conexión se estableció correctamente');
   } catch (error) {
     console.error(`No se pudo establecer la conexión correctamente: ${error}`);
   }
