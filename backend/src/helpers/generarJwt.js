ffc6ac28ac85d17ed14304e76ffd00ab37f55457
Gayoso Facundo <facusbrr@gmail.com>
2024-09-19 11:36:53 -0300

    Se creo el helper de generarJwt para poder generar un token

---
+++ backend/src/helpers/generarJwt.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { SECRET_KEY } from '../config/env.js';
+
+const generarJwt = async userId => {
+  try {
+    const payload = { userId };
+    const token = await new Promise((resolve, reject) => {
+      jwt.sign(payload, SECRET_KEY, { expiresIn: '5h' }, (error, token) => {
+        if (error) {
+          console.error('Error al generar el token:', error);
+          reject('No se pudo generar el token');
+        } else {
+          resolve(token);
+        }
+      });
+    });
+    return token;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default generarJwt;
