429ee83c72d530b35632c6ddf5128f6fcc29cd52
Gayoso Facundo <facusbrr@gmail.com>
2024-09-19 14:32:59 -0300

    Se fixeo el servicio de signUp para que maneje errores mas concretos

--- backend/src/components/auth/services/authService.js
+++ backend/src/components/auth/services/authService.js
@@ -7,30 +7,23 @@ import {
   createUser,
 } from '../../users/services/userServices.js';
 
-export const signUp = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const userExisting = getUserByEmail(email);
+export const signUp = async req => {
+  const { name, email, password } = req.body;
+  const userExisting = await getUserByEmail(email);
 
-    if (userExisting) {
-      return res.status(400).json({ message: 'El usuario ya existe' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await createUser({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    const token = generarJwt(newUser.id);
-
-    res.status(201).json({ token });
-  } catch (error) {
-    console.error(`Error al registrar al usuario: ${error}`);
-    res
-      .status(500)
-      .json({ message: 'Error en el servidor al registrar el usuario' });
+  if (userExisting) {
+    throw new Error('El usuario ya existe');
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await createUser({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = await generarJwt(newUser.id);
+
+  return { token };
 };
