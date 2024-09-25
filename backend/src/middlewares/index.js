db194eba6c948aa733125991b0ca5899dcf09642
Gayoso Facundo <facusbrr@gmail.com>
2024-09-25 12:33:21 -0300

    Se agrego al middleware de cors para que pueda recibir multiples origenes

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import { corsMiddleware } from './corsMiddleware.js';
 
 export const initMiddleware = app => {
-  app.use(cors(corsMiddleware()));
+  app.use(cors(corsMiddleware));
   app.use(express.json());
   app.use(cookieParser());
   app.use(morgan('dev'));
