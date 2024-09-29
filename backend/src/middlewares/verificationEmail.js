4d9d39e022a31300220c7df550f0a76397e54cc4
Facundo Gayoso <facusbrr@gmail.com>
2024-09-29 17:56:20 -0300

    Se agrego el middleware de requerir validacion de email para acceder a ciertas areas

---
+++ backend/src/middlewares/verificationEmail.js
@@ -0,0 +1,18 @@
+import { Users } from '../components/users/models/userModel';
+
+export const requireEmailVerification = async (req, res, next) => {
+  const userId = req.user.id; // Asumiendo que el ID del usuario está disponible en req.user
+
+  try {
+    const user = await Users.findByPk(userId);
+
+    if (!user.emailVerified) {
+      return res.status(403).json({ message: 'Debes verificar tu email para acceder a esta funcionalidad' });
+    }
+
+    next();
+  } catch (error) {
+    console.error(`Error al verificar el estado del email: ${error}`);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
