3a9abe432c7ee2831873d1388ddce1210b0e6dfd
Mateo <cuellamateo@gmail.com>
2024-09-30 15:55:29 -0300

    Correciones generales

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import { FaArrowTrendUp } from 'react-icons/fa6';
 
 export function LandingPage() {
   return (
-    <div>
+    <>
       {/* Navbar */}
       <Navbar />
 
@@ -13,15 +13,14 @@ export function LandingPage() {
       <Hero />
 
       {/* Features */}
-      <div className="flex w-3/5 mx-auto">
+      <div className="flex flex-wrap w-4/5 lg:w-3/5 mx-auto  ">
         <FeatureCard
-          icon={<MdAssignmentAdd className="fill-purple-600" />}
+          icon={<MdAssignmentAdd className="fill-purple-600 " />}
           title={'Creación de Materias Personalizadas'}
           description={
             'Organiza tu aprendizaje creando materias a medida. Adapta cada asignatura a tus necesidades y visualiza el progreso de tus tareas de forma clara y sencilla.'
           }
         />
-        <div className="divider divider-horizontal my-10"></div>
         <FeatureCard
           icon={<FaRegClock className="fill-purple-600" />}
           title={'Tareas Estructuradas con Pomodoro'}
@@ -29,18 +28,17 @@ export function LandingPage() {
             'Divide tus tareas en puntos manejables con nuestro sistema Pomodoro. Alterna entre períodos de trabajo y descansos cortos para mantener tu concentración y reducir la ansiedad.'
           }
         />
-        <div className="divider divider-horizontal my-10"></div>
         <FeatureCard
           icon={<FaArrowTrendUp className="fill-purple-600" />}
           title={'Mejora Tu Rendimiento Escolar'}
           description={
-            'Optimiza tu estudio con recordatorios y seguimiento del progreso. Diseñada especialmente para estudiantes con TDAH, nuestra app te ayuda a maximizar tu eficiencia y lograr tus objetivos académicos.'
+            'Optimiza tu estudio con recordatorios y seguimiento del progreso. Diseñada especialmente para estudiantes con TDAH, te ayudamos a maximizar tu eficiencia y lograr tus objetivos académicos.'
           }
         />
       </div>
 
       {/* Footer */}
       <Footer />
-    </div>
+    </>
   );
 }
