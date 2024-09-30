33d7becbc274692beaca7e1e6e232915eeb388a6
Mateo <cuellamateo@gmail.com>
2024-09-30 15:55:29 -0300

    Correciones generales

--- frontend/src/components/FeatureCard.jsx
+++ frontend/src/components/FeatureCard.jsx
@@ -1,7 +1,7 @@
 export const FeatureCard = ({ icon, title, description }) => {
   return (
-    <div className="flex flex-col p-4 max-w-xs mx-auto mt-5">
-      <div className="text-2xl mb-2">{icon}</div>
+    <div className="flex flex-col text-center md:text-start md:flex-1 p-4  mx-auto mt-5">
+      <div className="text-2xl mb-2 self-center md:self-start">{icon}</div>
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="font-semibold text-gray-600">{description}</p>
     </div>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { FaXTwitter } from 'react-icons/fa6';
 
 export const Footer = () => {
   return (
-    <footer className="footer  text-neutral p-10 mt-10">
+    <footer className="footer text-neutral justify-center md:justify-normal p-10 mt-10   lg:w-3/5 lg:mx-auto ">
       <aside>
         <h1 className="text-5xl font-bold text-purple-600 mb-2">Tidy</h1>
         <p>Copyright © 2024 - Todos los derechos reservados</p>
       </aside>
-      <nav>
+      <nav className="md:justify-self-center">
         <h6 className="footer-title">Social</h6>
         <div className="grid grid-flow-col gap-4">
           <a href="#">{<FaXTwitter className="text-3xl" />}</a>
