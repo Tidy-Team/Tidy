03f7cb5c83fcf2e0c4eb0d78bcf61a3182d8faaf
Mateo <cuellamateo@gmail.com>
2024-10-04 14:55:11 -0300

    Se comenzo con la seccion de actividades

--- frontend/src/context/useActvity.js
+++ frontend/src/context/useActvity.js
@@ -1,1 +1,10 @@
-export const useActivities = () => {};
+import { useContext } from 'react'
+import { ActivityContext } from './ActivityContext'
+
+export const useActivities = () => {
+  const context = useContext(ActivityContext)
+  if (!context) {
+    throw new Error('useActivities must be used within a ActivityProvider')
+  }
+  return context
+}
