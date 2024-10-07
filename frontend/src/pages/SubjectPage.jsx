485878d07da7b55be5b3cdcbebcb76472a65b82f
Mateo <cuellamateo@gmail.com>
2024-10-07 12:18:21 -0300

    Se trabajo en el aspecto responsive de la pagina materias

--- frontend/src/pages/SubjectPage.jsx
+++ frontend/src/pages/SubjectPage.jsx
@@ -1,8 +1,12 @@
-import { useParams } from 'react-router-dom'
-import { useSubjects } from '../context/useSubject.js'
+//React
 import { useEffect, useState } from 'react'
-import { Modal } from '../components/Modal.jsx'
-import { ActivityForm } from '../components/ActivityForm.jsx'
+import { useParams } from 'react-router-dom'
+
+//Components and utilities
+import { useSubjects } from '../context/useSubject.js'
+import { Modal, ActivityForm } from '../components'
+
+//Icons
 
 export function SubjectPage() {
   const { subject, getSubject } = useSubjects()
@@ -28,38 +32,38 @@ export function SubjectPage() {
   }
 
   return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-4 md:h-[calc(100vh-90px)] md:overflow-hidden max-h-screen ">
+    <div className="grid grid-cols-3  gap-4 md:h-[calc(100vh-90px)] max-h-full ">
       <div className="col-span-3 min-h-fit">
         <div className="flex flex-col-reverse p-5 h-48 w-full bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg">
           <h1 className="text-4xl font-semibold text-start ">{subject.name}</h1>
         </div>
       </div>
-      <div className="row-start-2 col-span-3 md:col-span-1  bg-base-300 text-center content-center rounded-md">
+      <div className="row-start-2 col-span-3 md:col-span-1  bg-base-300 text-center content-center rounded-md p-5 md:h-40">
         <p className="text-xl font-semibold">
           Profesores: {subject.name_teacher}
         </p>
         <p className="text-xl font-semibold"> {subject.description}</p>
       </div>
-      <div className="col-span-3 md:col-span-2 row-span-2 row-start-3 md:row-start-2 text-center ">
+      <div className="col-span-3 md:col-span-2 row-span-2 row-start-3 md:row-start-2 text-center  min-h-fit ">
         <div>
           <h1 className="text-2xl font-semibold bg-base-300 rounded-md p-3 mb-2">
             Tareas
           </h1>
         </div>
-        <div className="h-fit flex flex-col gap-2 overflow-y-auto md:max-h-full ">
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
-          <div className="p-5 bg-base-200 rounded-lg text-start  ">Tarea 1</div>
+        <div className="h-fit flex flex-col gap-2 md:mb-5 ">
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
+          <div className="p-5 bg-base-300 rounded-lg text-start  ">Tarea 1</div>
         </div>
       </div>
     </div>
