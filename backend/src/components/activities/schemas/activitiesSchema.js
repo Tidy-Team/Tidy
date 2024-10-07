7bcef4d7e802e668e476675a83dfb32e23c46c81
Facundo Gayoso <facusbrr@gmail.com>
2024-10-06 21:40:06 -0300

    Se mejoro la validacion del Zod en el schema de activities

--- backend/src/components/activities/schemas/activitiesSchema.js
+++ backend/src/components/activities/schemas/activitiesSchema.js
@@ -3,40 +3,32 @@ import moment from 'moment-timezone';
 
 const timeZone = 'America/Argentina/Buenos_Aires';
 
+const parseDate = date => {
+  const parsedDate = moment(date);
+  if (!parsedDate.isValid()) {
+    throw new Error(`Fecha no válida: ${date}`);
+  }
+  return moment.tz(parsedDate, timeZone).toDate();
+};
+
 export const activitiesSchema = z.object({
-  titulo: z.string().min(1, 'Debe tener título').max(100, 'El titulo debe tener menos de 100 caracteres'),
+  titulo: z.string().min(1, 'Debe tener título').max(100, 'El título debe tener menos de 100 caracteres'),
   description: z.string().optional(),
-  fecha_inicio: z
-    .string()
-    .default(() => moment().tz(timeZone).format()) // Establece la fecha actual en la zona horaria de Buenos Aires
-    .refine(date => {
-      const parsedDate = moment(date);
-      if (!parsedDate.isValid()) {
-        console.error(`fecha_inicio no es una fecha válida: ${date}`);
-        return false;
-      }
-      const zonedDate = moment.tz(parsedDate, timeZone).toDate();
-      return zonedDate <= new Date();
-    }, 'La fecha de inicio no puede ser en el futuro'),
-  fecha_fin: z
-    .string()
-    .default(() => moment().tz(timeZone).format()) // Establece la fecha actual en la zona horaria de Buenos Aires
-    .refine(date => {
-      const parsedDate = moment(date);
-      if (!parsedDate.isValid()) {
-        console.error(`fecha_fin no es una fecha válida: ${date}`);
-        return false;
-      }
-      const zonedDate = moment.tz(parsedDate, timeZone).toDate();
-      return zonedDate >= moment().startOf('day').toDate(); // Permite la fecha actual o futura
-    }, 'La fecha de fin no puede ser en el pasado'),
+  fecha_inicio: z.preprocess(
+    parseDate,
+    z.date().refine(date => date <= new Date(), {
+      message: 'La fecha de inicio no puede ser en el futuro',
+    })
+  ),
+  fecha_fin: z.preprocess(
+    parseDate,
+    z.date().refine(date => date >= moment().startOf('day').toDate(), {
+      message: 'La fecha de fin no puede ser en el pasado',
+    })
+  ),
   estado: z.enum(['pendiente', 'en_progreso', 'completada']).default('pendiente'),
-  prioridad_id: z
-    .number()
-    .int()
-    .positive()
-    .refine(id => [1, 2, 3].includes(id), {
-      message: 'Prioridad no válida',
-    }),
+  prioridad_id: z.enum([1, 2, 3], {
+    errorMap: () => ({ message: 'Prioridad no válida' }),
+  }),
   num_preguntas: z.number().int().positive().min(1, 'Debe haber al menos una pregunta'),
 });
