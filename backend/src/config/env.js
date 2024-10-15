755a422fc815c4df80724ac107005457d80370fd
Gayoso Facundo <facusbrr@gmail.com>
2024-10-15 14:08:02 -0300

    Se actualizo el README.md

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 //Databases
-export const DB_HOST = process.env.DB_HOST;
+export const DB_HOST = process.env.DB_HOST || 3000;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
