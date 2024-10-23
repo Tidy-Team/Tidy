0dbd55068936706af0cea73e07c898a829d943fb
Mateo <cuellamateo@gmail.com>
2024-10-23 16:08:33 -0300

    Se refactorizo la pagina de materias

--- frontend/src/pages/SubjectPage.jsx
+++ frontend/src/pages/SubjectPage.jsx
@@ -7,44 +7,69 @@ import { useFetch } from '../hooks/useFetch.js'
 import { Modal, ActivityForm, ActivitiesList } from '../components'
 
 //Icons
+import { HiDotsVertical } from 'react-icons/hi'
 
 export function SubjectPage() {
   const { id } = useParams()
 
   const {
-    fetchData,
+    fetchData: fetchSubjectData,
     data: subject,
-    error,
-    isLoading,
+    error: subjectError,
+    isLoading: isSubjectLoading,
   } = useFetch(
     `http://localhost:3000/subjects/${id}`,
     'GET',
-    null,
     { 'Content-Type': 'application/json' },
     { credentials: 'include' }
   )
 
-  const [activities, setActivities] = useState([])
+  const {
+    fetchData: fetchActivitiesData,
+    data: activities,
+    error: activitiesError,
+    isLoading: isActivitiesLoading,
+  } = useFetch(
+    `http://localhost:3000/activities/${id}`,
+    'GET',
+    { 'Content-Type': 'application/json' },
+    { credentials: 'include' }
+  )
+
+  const [localSubject, setLocalSubject] = useState(null)
+  const [localActivities, setLocalActivities] = useState([])
 
   useEffect(() => {
-    fetchData()
+    fetchSubjectData()
+    fetchActivitiesData()
   }, [])
 
-  const addActivity = (newActivity) => {
-    const activityData = newActivity.data.activity
-    if (activityData.id === undefined) {
-      console.error('New activity has undefined id:', activityData)
-      return
+  useEffect(() => {
+    if (subject) {
+      setLocalSubject(subject)
     }
-    setActivities([...activities, activityData])
+  }, [subject])
+
+  useEffect(() => {
+    if (activities) {
+      setLocalActivities(activities)
+    }
+  }, [activities])
+
+  const addActivity = (newActivity) => {
+    setLocalActivities((prevActivities) => [...prevActivities, newActivity])
   }
 
-  if (isLoading) {
+  if (isSubjectLoading || isActivitiesLoading) {
     return <div>Loading...</div>
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>
+  if (subjectError) {
+    return <div>Error: {subjectError.message}</div>
+  }
+
+  if (activitiesError) {
+    return <div>Error: {activitiesError.message}</div>
   }
 
   return (
@@ -52,15 +77,15 @@ export function SubjectPage() {
       <div className="col-span-3 min-h-fit">
         <div className="flex flex-col-reverse p-5 h-48 w-full bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg">
           <h1 className="text-4xl font-semibold text-start text ">
-            {subject?.subjectName}
+            {localSubject?.subjectName}
           </h1>
         </div>
       </div>
       <div className="row-start-2 col-span-3 md:col-span-1  bg-base-300 text-center content-center rounded-md p-5 md:h-40">
         <p className="text-xl font-semibold">
-          Profesores: {subject?.name_teacher}
+          Profesores: {localSubject?.name_teacher}
         </p>
-        <p className="text-xl font-semibold"> {subject?.description}</p>
+        <p className="text-xl font-semibold"> {localSubject?.description}</p>
       </div>
       <div className="col-span-3 md:col-span-2 row-span-2 row-start-3 md:row-start-2 text-center  min-h-fit ">
         <div>
@@ -70,7 +95,47 @@ export function SubjectPage() {
         </div>
         <div className="h-fit flex flex-col gap-2 md:mb-5 ">
           {/* Activities */}
-          <ActivitiesList />
+          {localActivities.length === 0 ? (
+            <h1 className="p-5">No hay tareas</h1>
+          ) : (
+            localActivities.map((activity) => (
+              <div
+                key={activity.id}
+                className="p-3 bg-base-300 rounded-lg text-start flex justify-between items-center"
+              >
+                {activity.titulo}
+                <div className="flex items-center gap-3">
+                  <button
+                    className="btn btn-sm btn-primary btn-circle"
+                    onClick={() => {
+                      console.log(activity.id)
+                    }}
+                  ></button>
+                  <div className="dropdown dropdown-left">
+                    <div
+                      tabIndex="0"
+                      role="button"
+                      className="btn btn-circle btn-sm mb-1"
+                    >
+                      <HiDotsVertical />
+                    </div>
+
+                    <ul
+                      tabIndex="0"
+                      className="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box  "
+                    >
+                      <li>
+                        <a>Edit</a>
+                      </li>
+                      <li>
+                        <a>Delete</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
       <Modal children={<ActivityForm addActivity={addActivity} />} />
