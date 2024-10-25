78d354102933bf8d7e19cd04c049833e87e09727
Mateo <cuellamateo@gmail.com>
2024-10-24 23:21:44 -0300

    Se modifico SubjectCard

--- frontend/src/components/subjects/SubjectCard.jsx
+++ frontend/src/components/subjects/SubjectCard.jsx
@@ -6,18 +6,20 @@ import { FaChalkboardTeacher } from 'react-icons/fa'
 
 export function SubjectCard({ id, title, description, teacher }) {
   const navigate = useNavigate()
+  const randomImageUrl = `https://picsum.photos/seed/${id}/620/366`
 
   return (
     <div
-      className="card card-compact bg-base-200 shadow-xl cursor-pointer"
+      className="card card-bordered  image-full w-96  bg-base-200 hover:shadow-lg cursor-pointer"
       onClick={() => navigate(`/subjects/${id}`)}
     >
       <figure>
-        <div className="h-48 w-full bg-gradient-to-r from-purple-500 to-blue-500"></div>
+        <img src={randomImageUrl} alt={title} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <div className="flex  gap-3">
+        <p>{description}</p>
+        <div className="flex gap-3">
           <FaChalkboardTeacher className=" text-2xl self-center" />
           <h3 className="font-semibold text-base ">{teacher}</h3>
         </div>
