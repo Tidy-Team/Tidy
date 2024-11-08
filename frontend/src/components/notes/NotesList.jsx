7494547e9043421b2db38644043e3292ea1a0105
Mateo <cuellamateo@gmail.com>
2024-11-08 16:20:58 -0300

    Rediseño de las notas

--- frontend/src/components/notes/NotesList.jsx
+++ frontend/src/components/notes/NotesList.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 import { useEffect, useState } from 'react'
 
+import { MdDelete, MdEdit } from 'react-icons/md'
+import { HiDotsVertical } from 'react-icons/hi'
+
 export const NotesList = () => {
   const { id } = useParams()
 
@@ -34,16 +37,54 @@ export const NotesList = () => {
     setLocalNotes([...localNotes, noteData])
   }
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center  mx-auto">
+        <span className="loading loading-spinner self-center loading-sm"></span>
+      </div>
+    )
+  }
+
   return (
-    <ul>
+    <div>
       {localNotes.length === 0 && (
         <div className="flex justify-center items-center p-10 m-10">
           <div>Vacio</div>
         </div>
       )}
       {localNotes.map((note) => {
-        return <li>{note.description}</li>
+        return (
+          <div
+            className=" flex items-center pb-3 overflow-visible"
+            key={note.id}
+          >
+            <p className="text-left w-full text-wrap">- {note.description}</p>
+            <div className="dropdown dropdown-left !z-[80]">
+              <div
+                tabIndex="0"
+                role="button"
+                className="btn btn-ghost btn-circle btn-sm mb-1"
+              >
+                <HiDotsVertical />
+              </div>
+
+              <ul
+                tabIndex="0"
+                className="dropdown-content z-[1] join join-vertical shadow bg-base-100 rounded-box w-28"
+              >
+                <button className="btn btn-ghost flex  join-item min-h-fit">
+                  <MdEdit className="text-lg" />
+                  Editar
+                </button>
+                <button className="btn btn-ghost join-item">
+                  <MdDelete />
+                  Eliminar
+                </button>
+              </ul>
+            </div>
+          </div>
+        )
       })}
-    </ul>
+    </div>
   )
 }
